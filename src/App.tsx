edd8cde7f2c5a093806bca660f096c092ae23fcd
SpaceBucks <spaceclive@gmail.com>
2023-04-04 17:24:53 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
